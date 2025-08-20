a19dc83aae1dbd5d4581a025bc84e6eddf68fef2
fiyinfoluwa84 <ffadeniyi1@gmail.com>
2025-08-20 12:31:03 +0100

    Update stock_data_fetcher.py
    This stage of the project involves the fetching of stock data(Open, High, Low, Close) OHLC for 5 companies using  Yahoo Finance API. (yfinance). The stock data for a duration of 1year was fetched.

--- data_ingestion/stock_data_fetcher.py
+++ data_ingestion/stock_data_fetcher.py
@@ -1,1 +1,7 @@
+# Extraction
 
+import pandas as pd
+import yfinance as yf
+tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA"]
+data = yf.download(tickers, period="1y")
+Data_columns=data[["Open", "High", "Low", "Close"]]
